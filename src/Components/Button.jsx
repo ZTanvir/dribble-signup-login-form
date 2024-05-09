a4243216b38effbfe728f2ce0819ba4f40ace9d9
ztanvir <zahirulopel@gmail.com>
2024-05-09 18:04:14 +0600

    Add style padding,border for button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,6 +1,8 @@
+import styles from "../Styles/button.module.css";
+
 const Button = ({ buttonIcon, buttonName }) => {
     return (
-        <button type="button">
+        <button className={styles.signUpBtn} type="button">
             <span>
                 <img src={buttonIcon} alt={`${buttonName} icon`} />
                 <span>{buttonName}</span>
